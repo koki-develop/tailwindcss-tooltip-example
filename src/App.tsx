95ae3612bb26af89bacbbd0df561846f99a95f01
koki-develop <kou.pg.0131@gmail.com>
2022-05-15 15:28:55 +0900

    ボタンを設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React, { memo } from "react";
 
 const App: React.FC = memo(() => {
-  return <div>hello world</div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <button className="rounded border bg-white px-2 py-1 shadow transition hover:bg-gray-100">
+        Button
+      </button>
+    </div>
+  );
 });
 
 App.displayName = "App";
