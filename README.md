4e4b9b423eb357a77356d3fd0468983c73a4e743
koki-develop <kou.pg.0131@gmail.com>
2022-05-15 22:40:23 +0900

    リポジトリ名を修正

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Tailwind CSS でツールチップを作成するサンプルコード。
 
-- [デモ](https://koki-develop.github.io/tailwind-tooltip-example/)
+- [デモ](https://koki-develop.github.io/tailwindcss-tooltip-example/)
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tailwind-tooltip-example/",
+  base: "/tailwindcss-tooltip-example/",
   plugins: [react()],
 });
