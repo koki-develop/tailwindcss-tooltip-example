41fb986b53ee8f58c9ba80264b58c71073ea25c9
koki-develop <kou.pg.0131@gmail.com>
2022-05-15 15:42:42 +0900

    tooltip をコンポーネント化

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React, { memo } from "react";
+import Tooltip from "./Tooltip";
 
 const App: React.FC = memo(() => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <span className="group relative">
-        <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black px-2 py-1 text-white opacity-0 transition before:absolute before:-bottom-1 before:right-0 before:left-0 before:m-auto before:w-0 before:border-4 before:border-b-0 before:border-transparent before:border-t-black before:content-[''] group-hover:opacity-100">
-          Hello World
-        </span>
-
+      <Tooltip text="Hello World">
         <button className="rounded border bg-white px-2 py-1 shadow transition hover:bg-gray-100">
           Button
         </button>
-      </span>
+      </Tooltip>
     </div>
   );
 });
