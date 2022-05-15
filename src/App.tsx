30fac0a59d57320cc2fe3f98485a5e690aade03c
koki-develop <kou.pg.0131@gmail.com>
2022-05-15 15:38:06 +0900

    tooltip の下に三角をつけた

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App: React.FC = memo(() => {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <span className="relative">
-        <span className="absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black px-2 py-1 text-white">
+        <span className="absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black px-2 py-1 text-white before:absolute before:-bottom-1 before:right-0 before:left-0 before:m-auto before:w-0 before:border-4 before:border-b-0 before:border-transparent before:border-t-black before:content-['']">
           Hello World
         </span>
 
