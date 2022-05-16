42fb0eb32f2f12a3b5947a99b144a58d387634c8
koki-develop <kou.pg.0131@gmail.com>
2022-05-16 16:32:57 +0900

    before の設定を修正

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -8,7 +8,7 @@ export type TooltipProps = {
 const Tooltip: React.FC<TooltipProps> = memo((props) => {
   return (
     <span className="group relative">
-      <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black px-2 py-1 text-white opacity-0 transition before:absolute before:-bottom-1 before:right-0 before:left-0 before:m-auto before:w-0 before:border-4 before:border-b-0 before:border-transparent before:border-t-black before:content-[''] group-hover:opacity-100">
+      <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black px-2 py-1 text-white opacity-0 transition before:absolute before:left-1/2 before:top-full before:-translate-x-1/2 before:border-4 before:border-transparent before:border-t-black before:content-[''] group-hover:opacity-100">
         {props.text}
       </span>
 
